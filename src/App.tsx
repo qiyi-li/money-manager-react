682c6debc5a63c79a586f31c18deb770beaa93b0
liubai <errgou@gmail.com>
2020-05-30 18:40:42 +0800

    创建 views

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import {
   Route,
   Redirect,
 } from 'react-router-dom';
-import Layout from 'components/Layout';
+import Money from './views/Money';
+import Statistics from './views/Statistics';
+import Tags from './views/Tags';
+import NoMatch from './views/NoMatch';
 
 function App() {
   return (
@@ -29,34 +32,6 @@ function App() {
   );
 }
 
-function NoMatch() {
-  return (
-    <div>访问页面不存在</div>
-  );
-}
 
-function Statistics() {
-  return (
-    <Layout>
-      <h2>Statistics</h2>
-    </Layout>
-  );
-}
-
-function Tags() {
-  return (
-      <Layout>
-        <h2>Tags</h2>
-      </Layout>
-  );
-}
-
-function Money() {
-  return (
-      <Layout>
-        <h2>Money</h2>
-      </Layout>
-  );
-}
 
 export default App;
