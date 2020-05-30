38e133052ccddb6ad967621966818efe13b438a1
liubai <errgou@gmail.com>
2020-05-30 22:36:38 +0800

    Money.tsx 中 完成 备注 部分的样式

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
-import {Link, NavLink} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import React from 'react';
 import Icon from './Icon';
 
 
 const NavWrapper = styled.nav`
+background-color:#fff;
 line-height: 24px;
 box-shadow: 0 0 3px rgba(0,0,0,0.25);
   >ul{
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -26,7 +26,21 @@ const TagsSection = styled.section`
   }
 `;
 const NotesSection = styled.section`
-
+  background-color: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+  >label{
+  display: flex;
+  align-items: center;
+    >span{ margin-right: 16px; white-space: nowrap;}
+    >input{
+      display: block;
+      width: 100%;
+      height: 72px;
+      border: none;
+      background:none;
+    }
+  }
 `;
 const CategorySection = styled.section`
 
@@ -51,7 +65,7 @@ function Money() {
       <NotesSection>
         <label>
           <span>备注</span>
-          <input type="text"/>
+          <input type="text" placeholder="在这里添加备注"/>
         </label>
       </NotesSection>
       <CategorySection>
