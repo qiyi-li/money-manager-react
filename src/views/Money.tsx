fafa6d41d3a5cdd90d259c149ce934958e19b7c2
liubai <errgou@gmail.com>
2020-05-30 21:41:59 +0800

    Money.tsx 中 完成 TagsSection 部分的样式

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,10 +1,86 @@
 import Layout from '../components/Layout';
 import React from 'react';
+import styled from 'styled-components';
+
+const TagsSection = styled.section`
+  background-color:#fff;
+  padding: 12px 16px;
+  >ol{
+    margin: 0 -12px;
+    >li{
+      background-color:#d9d9d9;
+      border-radius: 18px;
+      display: inline-block;
+      padding: 3px 18px;
+      font-size: 14px;
+      margin: 8px 12px;
+    }
+  }
+  >button{
+    background:none;
+    border: none;
+    border-bottom: 1px solid #333;
+    padding: 2px 4px;
+    color: #666;
+    margin-top: 8px;
+  }
+`;
+const NotesSection = styled.section`
+
+`;
+const CategorySection = styled.section`
+
+`;
+const NumberPadSection = styled.section`
+
+`;
+
 
 function Money() {
   return (
     <Layout>
-      <h2>Money</h2>
+      <TagsSection>
+        <ol>
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+        </ol>
+        <button>新增标签</button>
+      </TagsSection>
+      <NotesSection>
+        <label>
+          <span>备注</span>
+          <input type="text"/>
+        </label>
+      </NotesSection>
+      <CategorySection>
+        <ul>
+          <li>支出</li>
+          <li>收入</li>
+        </ul>
+      </CategorySection>
+      <NumberPadSection>
+        <div>100</div>
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>AC</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>+</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>-</button>
+          <button>.</button>
+          <button>0</button>
+          <button>del</button>
+          <button>ok</button>
+        </div>
+      </NumberPadSection>
     </Layout>
   );
 }
