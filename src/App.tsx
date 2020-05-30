562d38c24745ab68dabeb25f331f80e0a4af6173
liubai <errgou@gmail.com>
2020-05-30 11:14:47 +0800

    初步设定 Nav 样式

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,19 @@ import {
   Link
 } from 'react-router-dom';
 import styled from 'styled-components';
+import Nav from './components/Nav';
 
 const Wrapper = styled.div`
-  border: 1px solid orangered;
   height: 100vh;
   display: flex;
   flex-direction: column;
+  
 `;
 const Main = styled.div`
   flex-grow: 1;
   overflow: auto;
 `;
-const Nav=styled.nav`
-  >ul{
-    display: flex;
-    >li{
-      width: 33.333333%;
-      text-align: center;
-      padding: 16px;
-    }
-  }
-`
+
 
 function App() {
   return (
@@ -50,19 +42,7 @@ function App() {
             </Route>
           </Switch>
         </Main>
-        <Nav>
-          <ul>
-            <li>
-              <Link to="/money">记账页</Link>
-            </li>
-            <li>
-              <Link to="/tags">标签页</Link>
-            </li>
-            <li>
-              <Link to="/statistics">统计页</Link>
-            </li>
-          </ul>
-        </Nav>
+        <Nav/>
       </Wrapper>
     </Router>
   );
