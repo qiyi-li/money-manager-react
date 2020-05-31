6e079f70186297c11ed39311167d8f0426d26293
liubai <errgou@gmail.com>
2020-05-31 09:12:25 +0800

    Money.tsx 中完成 收入支出类型 部分的样式

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -43,7 +43,34 @@ const NotesSection = styled.section`
   }
 `;
 const CategorySection = styled.section`
-
+  font-size: 24px;
+  >ul{
+    display: flex;
+    background:#c4c4c4;
+    >li{
+      width: 50%;
+      text-align: center;
+      padding: 16px 0;
+      position: relative;
+      >.underline{
+        height: 3px;
+        background-color:#fd6600;
+        position: absolute;
+        width: 0;
+        left: 50%;
+        bottom: 0;
+        transform: translate(-50%);
+      }
+      &.selected{
+        background-color:#fff;
+        color: #fd6600;
+        >.underline{
+          width: 100%;
+          transition: width .3s;
+        }
+        }
+    }
+  }
 `;
 const NumberPadSection = styled.section`
 
@@ -70,7 +97,10 @@ function Money() {
       </NotesSection>
       <CategorySection>
         <ul>
-          <li>支出</li>
+          <li className="selected">
+            支出
+            <div className="underline"/>
+          </li>
           <li>收入</li>
         </ul>
       </CategorySection>
