606c03c878d843d4827b16c2b751ae6a3aeaf4dc
liubai <errgou@gmail.com>
2020-06-07 22:07:39 +0800

    创建 createId.tsx , 让 id 自增

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,10 +1,13 @@
 import {useState} from 'react';
+import {createId} from 'lib/createId';
+// import {Id} from './lib/Id';
 const useTags = () => { //如此为： 封装一个自定义 Hook
   const [tags, setTags] = useState<{ id: number; name: string }[]>([
-    {id: 1, name: '衣'},
-    {id: 2, name: '食'},
-    {id: 3, name: '住'},
-    {id: 4, name: '行'},
+    // {id: (new Id).value, name: '食'},
+    {id: createId(), name: '食'},
+    {id: createId(), name: '食'},
+    {id: createId(), name: '住'},
+    {id: createId(), name: '行'},
   ]);
   return {tags: tags, setTags: setTags};
 };
