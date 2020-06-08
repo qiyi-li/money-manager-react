f1ba38f451d8372ffa0940e962687d3077dbb110
liubai <errgou@gmail.com>
2020-06-08 21:15:00 +0800

    封装 Butto

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  font-size: 18px;
+  color: #fff;
+  border: none;
+  padding: 8px 12px;
+  background-color:#fd6600;
+  border-radius: 4px ;
+`;
+export{Button}
