3cc0cebe679dce831326d97e1e7824cd9a7702fd
liubai <errgou@gmail.com>
2020-06-08 21:56:56 +0800

    实现编辑 tag 名

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -11,7 +11,27 @@ const useTags = () => { //如此为： 封装一个自定义 Hook
   const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags
     // {id: (new Id).value, name: '食'},
   );
-  const findTag=(id:number)=> tags.filter(tag=>tag.id===id)[0]
-  return {tags: tags, setTags: setTags,findTag};
+  const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
+  const findTagIndex = (id: number) => {
+    let result = -1;
+    for (let i = 0; i < tags.length; i++) {
+      if (tags[i].id === id) {
+        result = i;
+        break;
+      }
+    }
+    return result;
+  };
+  const updateTag = (id: number, obj: { name: string }) => {
+    //获取需要更改的 tag 下标
+    const index = findTagIndex(id);
+    // 深拷贝 tags 得到 tagsClone
+    const tagsClone = JSON.parse(JSON.stringify(tags));
+    // 把 tagsClone 的第 index 删掉，替换为 {id:id , name:obj,name}
+    tagsClone.splice(index, 1, {id: id, name: obj.name}); // 返回的是被删除的那一项
+    // 最后得到的就是 预期中的 tags
+    setTags(tagsClone);
+  };
+  return {tags: tags, setTags: setTags, findTag, updateTag, findTagIndex};
 };
 export {useTags}
--- src/views/EditTag.tsx
+++ src/views/EditTag.tsx
@@ -26,18 +26,25 @@ const Topbar = styled.header`
   background-color:#fff;
 `;
 const EditTag:React.FC = ()=>{
-  const {findTag}=useTags();
-  let{id}=useParams<Params>();
-  const tag=findTag(parseInt(id));
+  const {findTag,updateTag}=useTags();
+  let{id :idString}=useParams<Params>();
+  const tag=findTag(parseInt(idString));
   return (
     <Layout>
       <Topbar>
-        <Icon name="left"/>i
+        <Icon name="left"/>
         <span>编辑标签</span>
         <Icon name="submit"/>
       </Topbar>
       <InputWrapper>
-        <Input label="标签名" type="text"  placeholder="标签名" value={tag.name}/>
+        <Input label="标签名"
+               type="text"
+               placeholder="标签名"
+               value={tag.name}
+               onChange={(e)=>{
+                updateTag(tag.id,{name:e.target.value})
+               }}
+        />
       </InputWrapper>
       <Center>
         <Space/>
