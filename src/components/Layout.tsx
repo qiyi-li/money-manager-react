532e0c008370677e447a1a033f04f4ecb2074efb
a-lei-7 <qiyi.li@aol.com>
2020-06-12 12:29:06 +0800

    添加çª图片提示

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,6 @@
 import Nav from './Nav';
 import React, {useEffect, useRef} from 'react';
 import styled from 'styled-components';
-import {useRecords} from '../hooks/useRecords';
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -35,6 +34,17 @@ const Layout: React.FC<Props> = (props) => {
     </Wrapper>
   );
 };
+if (document.documentElement.clientWidth > 500) {
+  window.alert('为保证浏览效果，请用手机打开此页面');
+  const img = document.createElement('img',);
+  img.src = './qrcode-r.png';
+  img.style.position = 'fixed';
+  img.style.left = '50%';
+  img.style.top = '50%';
+  img.style.transform = 'translate(-50%,-50%)';
+  img.style.boxShadow='0 0 10px rgba(0,0,0,0.25)'
+  document.body.appendChild(img);
+}
 
 Layout.defaultProps={
   scrollTop:0
